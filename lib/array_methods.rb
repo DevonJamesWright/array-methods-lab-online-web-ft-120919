8130a8b144c52e160ae36b456d252f3a54def58a
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-10 17:17:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,16 @@
 def using_include(array, element)
 array = [1,2,3,4]
-array.include? [0]
+array.include? [1]
 end
 
 def using_sort(array)
-array = [2,4,5,3,7]
+array = [2,4,5,3,"wow"]
 array.sort
 end
 
 def using_reverse(array)
-
+array = [1,2,4,5,65]
+array.reverse
 end
 
 def using_first(array)
